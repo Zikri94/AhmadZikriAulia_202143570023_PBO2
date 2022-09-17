981bcb750cae6e0a61dac2096b4f84d99fb49733
Zikri94 <zikriaulia72@gmail.com>
2022-09-17 12:52:30 +0700

    Done

---
+++ PBO2_Manusia.java
@@ -0,0 +1,30 @@
+package javaapplication1;
+
+public class Manusia {
+String rambut, mata, mulut, telinga, tangan, kaki;
+
+String bermain(String x){
+return x;
+}
+
+String makan(String x){
+return x;
+}
+
+Boolean tidur(Boolean x){
+return x;
+}
+
+String bekerja(String x){
+return x;
+}
+
+String memasak(String x){
+return x;
+}
+
+String mencuci(String x){
+return x;
+}
+
+}
---
+++ PBO2_PesawatTelepon.java
@@ -0,0 +1,9 @@
+package javaapplication1;
+
+public class PesawatTelepon {
+String bentuk, kabel, gagang_telepon;
+
+terima_panggilan();
+melakukan_panggilan();
+
+}
